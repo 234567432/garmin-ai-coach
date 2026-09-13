5a69973816eecb8770252ff67a1e7fe1e1561a1e
234567432 <100158860+234567432@users.noreply.github.com>
2026-09-13 12:15:34 +0200

    Update model_config.py

--- services/ai/model_config.py
+++ services/ai/model_config.py
@@ -244,8 +244,8 @@ class ModelSelector:
                 temperature=0.2,
                 num_ctx=16384,
                 num_predict=8192, #neu eingefügt, für bessere Hardware anpassen
-                repeat_penalty=1.2, # NEU: Verhindert textliche Wiederholungsschleifen
-                format="json"  # <-- WICHTIG: Aktiviert die native strukturierte JSON-Ausgabe
+                repeat_penalty=1.2 # NEU: Verhindert textliche Wiederholungsschleifen
+#                format="json"  # <-- WICHTIG: Aktiviert die native strukturierte JSON-Ausgabe
             )
         # ---------------------------------
 
